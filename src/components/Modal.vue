970396f3fab961672751c67805a80c13ec6cb410
nursyahjaya <nursyahjaya@gmail.com>
2022-12-01 14:59:05 +0700

    update page tanaman and panel surya

--- src/components/Modal.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="left-0 flex h-screen  top-0 bg-white justify-center items-center z-50">
-    <slot></slot>
-  </div>
-</template>
--- src/supabase.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="./env" />
-
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
